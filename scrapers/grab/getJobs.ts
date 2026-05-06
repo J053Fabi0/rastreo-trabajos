d3f785180c87844fe881bae006229d635f859de7
Jose Fabio <J053Fabi0@users.noreply.github.com>
2026-05-06 07:30:45 -0500

    Arreglar el uso de page en getJobsPage

--- scrapers/grab/getJobs.ts
+++ scrapers/grab/getJobs.ts
@@ -1,8 +1,19 @@
 import getJobsPage from "./getJobsPage.ts";
+import Job from "../../data/models/job.model.ts";
 
-export default async function getJobs() {
-  const { jobs } = await getJobsPage(1);
-  return jobs;
+export default async function getJobs(): Promise<Job[]> {
+  const jobsByUrl = new Map<string, Job>();
+
+  let page = 1;
+  do {
+    const { jobs, hasNext } = await getJobsPage(page++);
+
+    const prevSize = jobsByUrl.size;
+    for (const job of jobs) jobsByUrl.set(job.url, job);
+
+    const change = jobsByUrl.size - prevSize;
+    if (hasNext === false || change !== jobs.length) break;
+  } while (true);
+
+  return Array.from(jobsByUrl.values());
 }
-
-await getJobs();
--- scrapers/grab/getJobsPage.ts
+++ scrapers/grab/getJobsPage.ts
@@ -1,3 +1,4 @@
+import { curl } from "@spawn/curl";
 import { DOMParser } from "@b-fuze/deno-dom";
 import Job, { Company } from "../../data/models/job.model.ts";
 
@@ -8,11 +9,6 @@ interface ToReturn {
 
 const pagesize = 20;
 
-/**
- * Por algún motivo, page no funciona, siempre es como si fuera 1
- * Incluso copiando el fetch original con headers y cookies
- * Pero con curl en la terminal sí funciona, sin necesidad de ningún header
- */
 export default async function getJobsPage(page: number): Promise<ToReturn> {
   const url = new URL("https://www.grab.careers/en/jobs");
   url.searchParams.set("team", "Engineering");
@@ -20,7 +16,11 @@ export default async function getJobsPage(page: number): Promise<ToReturn> {
   url.searchParams.set("pagesize", pagesize.toString());
   if (page >= 2) url.searchParams.set("page", page.toString());
 
-  const htmlString = await (await fetch(url.toString())).text();
+  const htmlString = await curl([
+    url.toString(),
+    "-H",
+    "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+  ]).text();
 
   const dom = new DOMParser().parseFromString(htmlString, "text/html");
 
@@ -41,5 +41,7 @@ export default async function getJobsPage(page: number): Promise<ToReturn> {
       });
   }
 
-  return { jobs, hasNext: jobElements.length >= pagesize };
+  const isAll = jobElements.length < pagesize || Boolean(dom.querySelector("ul.pagination > li.next")) === false;
+
+  return { jobs, hasNext: isAll === false };
 }
